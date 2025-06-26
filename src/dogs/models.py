575609d6257e971f0e97bfd58caa177be2b2485c
ivangor246 <ivangor246@gmail.com>
2025-06-26 13:53:25 +0300

    Add breed model

--- src/dogs/models.py
+++ src/dogs/models.py
@@ -1,3 +1,28 @@
+from django.core.validators import MaxValueValidator, MinValueValidator
 from django.db import models
 
-# Create your models here.
+
+class Breed(models.Model):
+    """Represents a dog breed with characteristic.
+
+    Attributes:
+        name (str): The name of the breed.
+        size (str): The size category.
+        friendliness (int): How friendly the breed is, from 1 to 5.
+        trainability (int): How easy the breed is to train, from 1 to 5.
+        shedding_amount (int): How much shedding, from 1 to 5.
+        exercise_needs (int): Exercise requirements, from 1 to 5.
+    """
+
+    class Sizes(models.TextChoices):
+        TINY = 't', 'Tiny'
+        SMALL = 's', 'Small'
+        MEDIUM = 'm', 'Medium'
+        LARGE = 'l', 'Large'
+
+    name = models.CharField(max_length=127)
+    size = models.CharField(max_length=1, choices=Sizes.choices, default=Sizes.MEDIUM)
+    friendliness = models.IntegerField(validators=[MinValueValidator(1), MaxValueValidator(5)])
+    trainability = models.IntegerField(validators=[MinValueValidator(1), MaxValueValidator(5)])
+    shedding_amount = models.IntegerField(validators=[MinValueValidator(1), MaxValueValidator(5)])
+    exercise_needs = models.IntegerField(validators=[MinValueValidator(1), MaxValueValidator(5)])
