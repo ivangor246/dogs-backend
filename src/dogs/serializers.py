343843bc89f737f97f0405d69d2719f62fcaedcc
ivangor246 <ivangor246@gmail.com>
2025-06-28 23:39:08 +0300

    Add docs for breed serializer

--- src/dogs/serializers.py
+++ src/dogs/serializers.py
@@ -4,6 +4,8 @@ from dogs.models import Breed
 
 
 class BreedSerializer(serializers.ModelSerializer):
+    """Serializer for the Breed model."""
+
     dog_count = serializers.IntegerField(default=0, read_only=True)
 
     class Meta:
