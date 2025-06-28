b1e75edadfc53e52d22fcfabf01e41f07d2d6aed
ivangor246 <ivangor246@gmail.com>
2025-06-28 22:07:16 +0300

    Add breed serializer

---
+++ src/dogs/serializers.py
@@ -0,0 +1,20 @@
+from rest_framework import serializers
+
+from dogs.models import Breed
+
+
+class BreedSerializer(serializers.ModelSerializer):
+    dog_count = serializers.IntegerField(default=0, read_only=True)
+
+    class Meta:
+        model = Breed
+        fields = (
+            'id',
+            'name',
+            'size',
+            'friendliness',
+            'trainability',
+            'shedding_amount',
+            'exercise_needs',
+            'dog_count',
+        )
