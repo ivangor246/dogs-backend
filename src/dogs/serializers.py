5c155923cdc25d03e8b1a1bd9365bcf91ac29985
ivangor246 <ivangor246@gmail.com>
2025-06-29 17:07:54 +0300

    Add dog serializer

--- src/dogs/serializers.py
+++ src/dogs/serializers.py
@@ -1,6 +1,6 @@
 from rest_framework import serializers
 
-from dogs.models import Breed
+from dogs.models import Breed, Dog
 
 
 class BreedSerializer(serializers.ModelSerializer):
@@ -19,4 +19,26 @@ class BreedSerializer(serializers.ModelSerializer):
             'shedding_amount',
             'exercise_needs',
             'dog_count',
+        )
+
+
+class DogSerializer(serializers.ModelSerializer):
+    """Serializer for the Dog model."""
+
+    avg_age = serializers.IntegerField(read_only=True)
+    same_breed_count = serializers.IntegerField(read_only=True)
+
+    class Meta:
+        model = Dog
+        fields = (
+            'id',
+            'name',
+            'age',
+            'breed',
+            'gender',
+            'color',
+            'favorite_food',
+            'favorite_toy',
+            'avg_age',
+            'same_breed_count',
         )
