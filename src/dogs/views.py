c01bc9d62acdde6b6dc8873f7169352014f00cb9
ivangor246 <ivangor246@gmail.com>
2025-06-29 13:32:35 +0300

    Add view for breeds

--- src/dogs/views.py
+++ src/dogs/views.py
@@ -1,3 +1,17 @@
-from django.shortcuts import render
+from django.db.models import Count
+from rest_framework.viewsets import ModelViewSet
 
-# Create your views here.
+from dogs.models import Breed
+from dogs.serializers import BreedSerializer
+
+
+class BreedViewSet(ModelViewSet):
+    """Introduces CRUD methods for interacting with breeds.
+
+    Attributes:
+        queryset (Queryset): A queryset of all breeds, annotated with dog count and ordered by id.
+        serializer_class (Type[Serializer]): The serializer class for breed instances.
+    """
+
+    queryset = Breed.objects.all().annotate(dog_count=Count('dogs')).order_by('id')
+    serializer_class = BreedSerializer
