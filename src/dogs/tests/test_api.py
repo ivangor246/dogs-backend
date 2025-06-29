4fe37f82d2ec17dc9744246674a163d49916a7f3
ivangor246 <ivangor246@gmail.com>
2025-06-29 13:33:25 +0300

    Add test for breed view

---
+++ src/dogs/tests/test_api.py
@@ -0,0 +1,97 @@
+from django.db.models import Count
+from django.urls import reverse
+from rest_framework import status
+from rest_framework.test import APITestCase
+
+from dogs.models import Breed, Dog
+from dogs.serializers import BreedSerializer
+
+
+class BreedViewSetTestCase(APITestCase):
+    def setUp(self):
+        self.breed_1 = Breed.objects.create(
+            name='Breed 1',
+            size='t',
+            friendliness=1,
+            trainability=1,
+            shedding_amount=1,
+            exercise_needs=1,
+        )
+        self.breed_2 = Breed.objects.create(
+            name='Breed 2',
+            size='s',
+            friendliness=2,
+            trainability=2,
+            shedding_amount=2,
+            exercise_needs=2,
+        )
+
+    def test_get(self):
+        url = reverse('breed-list')
+        response = self.client.get(url)
+        serialized_data = BreedSerializer([self.breed_1, self.breed_2], many=True).data
+        self.assertEqual(status.HTTP_200_OK, response.status_code)
+        self.assertEqual(serialized_data, response.data)
+
+    def test_get_with_dogs(self):
+        Dog.objects.create(
+            name='Dog',
+            age=10,
+            breed=self.breed_1,
+            gender='m',
+            color='black',
+            favorite_food='Some food',
+            favorite_toy='Some toy',
+        )
+
+        url = reverse('breed-list')
+        response = self.client.get(url)
+        breed_1_with_dogs = Breed.objects.annotate(dog_count=Count('dogs')).get(id=self.breed_1.id)
+        serialized_data = BreedSerializer([breed_1_with_dogs, self.breed_2], many=True).data
+        self.assertEqual(status.HTTP_200_OK, response.status_code)
+        self.assertEqual(serialized_data, response.data)
+
+    def test_get_one(self):
+        url = reverse('breed-detail', args=[self.breed_1.id])
+        response = self.client.get(url)
+        serialized_data = BreedSerializer(self.breed_1).data
+        self.assertEqual(status.HTTP_200_OK, response.status_code)
+        self.assertEqual(serialized_data, response.data)
+
+    def test_create(self):
+        url = reverse('breed-list')
+        data = {
+            'name': 'Breed 3',
+            'size': 'm',
+            'friendliness': 3,
+            'trainability': 3,
+            'shedding_amount': 3,
+            'exercise_needs': 3,
+        }
+        response = self.client.post(url, data, format='json')
+        self.assertEqual(status.HTTP_201_CREATED, response.status_code)
+        self.assertEqual(Breed.objects.count(), 3)
+        self.assertEqual(Breed.objects.last().name, 'Breed 3')
+
+    def test_update(self):
+        url = reverse('breed-detail', args=[self.breed_1.id])
+        updated_data = {
+            'name': 'Updated breed',
+            'size': 'l',
+            'friendliness': 2,
+            'trainability': 2,
+            'shedding_amount': 1,
+            'exercise_needs': 1,
+        }
+        response = self.client.put(url, updated_data, format='json')
+        self.breed_1.refresh_from_db()
+        self.assertEqual(status.HTTP_200_OK, response.status_code)
+        self.assertEqual(self.breed_1.name, updated_data['name'])
+        self.assertEqual(self.breed_1.size, updated_data['size'])
+        self.assertEqual(self.breed_1.friendliness, updated_data['friendliness'])
+
+    def test_delete(self):
+        url = reverse('breed-detail', args=[self.breed_1.id])
+        response = self.client.delete(url)
+        self.assertEqual(status.HTTP_204_NO_CONTENT, response.status_code)
+        self.assertFalse(Breed.objects.filter(id=self.breed_1.id).exists())
